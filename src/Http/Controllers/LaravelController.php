4e966de2e16a0997b5d79ffc7916a47d82adea76
Victor <mbamaravictor@gmail.com>
2021-06-03 20:32:28 +0100

    add controllers

---
+++ src/Http/Controllers/LaravelController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodnificent\JobWatcher\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class LaravelController extends Controller
+{
+    //
+}
---
+++ src/Http/Controllers/LumenController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodnificent\JobWatcher\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class LumenController extends BaseController
+{
+    //
+}
